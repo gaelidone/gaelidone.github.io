6c3debb3f28a761adf3062c9b1adef51267edd1c
gaelidone <gaelidonesarmiento@gmail.com>
2024-03-19 11:58:47 -0300

    Se terminó diseño de carrito y se cambió código JS

--- js/app.js
+++ js/app.js
@@ -12,94 +12,48 @@ categorias.forEach((categ) => {
       }
       categ.firstElementChild.classList.replace("bi-caret-right-square", "bi-caret-down-square-fill");
       categ.classList.add('active');
+      h2.innerHTML = categ.lastElementChild.innerHTML;
    })
 })
 
-
-
+crearProductos();
 categorias.forEach(categ => {
-   categ.addEventListener('click', () => {
-      click = mostrarProductos(categ)
-      if (click === 'todo') {
-         containProducs.innerHTML = "";
-         crearProductos(camisetasObj);
-         crearProductos(campesObj);
-         crearProductos(lompasObj);
-         crearProductos(zapasObj);
-         h2.innerHTML = "Todos los productos"
-
-      }else if (click === 'camisetas') {
-         containProducs.innerHTML = "";
-         crearProductos(camisetasObj);
-         h2.innerHTML = "Camisetas"
-
-      }else if (click === "campes") {
-         containProducs.innerHTML = "";
-         crearProductos(campesObj);
-         h2.innerHTML = "Camperas"
-
-      }else if (click === "lompas") {
-         containProducs.innerHTML = "";
-         crearProductos(lompasObj);
-         h2.innerHTML = "Pantalones"
-
-      }else if (click === "zapas") {
-         containProducs.innerHTML = "";
-         crearProductos(zapasObj);
-         h2.innerHTML = "Zapatillas"
-      }
+   categ.addEventListener('click',()=>{
+      let idCateg = categ.id;
+      containProducs.innerHTML = "";
+      crearProductos(idCateg);
    })
-})
+});
 
 
-const mostrarProductos = (categ) => {
-   let idCateg = categ.id;
-   if (idCateg === "todo") {
-      return idCateg;
-   } else if (idCateg === "camisetas") {
-      return idCateg;
-   } else if (idCateg === "campes") {
-      return idCateg;
-   } else if (idCateg === "lompas") {
-      return idCateg;
-   } else if (idCateg === "zapas") {
-      return idCateg;
+
+function crearProductos(idCateg){
+   let prodElegidos = productosObj.filter(prod => prod.categoria === idCateg)
+   if (prodElegidos.length === 0) {
+      prodElegidos = productosObj.map(prod => prod)
    }
+   prodElegidos.forEach(producto =>{
+      let newProducto = `
+        <div class="producto">
+           <div class="wrap-produc_img">
+            <img src="${producto.imagen}" alt="" class="producto-img">
+         </div>
+         <div class="producto-detalles">
+            <h3 class="producto-titulo">${producto.titulo}</h3>
+            <div class="wrap-precioBoton">                  
+               <p class="producto-precio">$${producto.precio}</p>
+               <button class="producto-agregar">AGREGAR</button>
+            </div>
+           </div>
+        </div>`;
+        containProducs.innerHTML += newProducto;
+
+      aside.classList.remove("menu-toggle");
+      glass.classList.add('glass-hidden');
+   })
 }
 
 
 
 
 
-function crearProductos(categoria) {
-   categoria.forEach(producto => {
-         let newProducto = `
-            <div class="producto">
-               <div class="wrap-produc_img">
-                  <img src="${producto.imagen}" alt="" class="producto-img">
-               </div>
-               <div class="producto-detalles">
-                  <h3 class="producto-titulo">${producto.titulo}</h3>
-                  <div class="wrap-precioBoton">                  
-                     <p class="producto-precio">$${producto.precio}</p>
-                     <button class="producto-agregar">AGREGAR</button>
-                  </div>
-               </div>
-            </div>`;
-         
-         containProducs.innerHTML += newProducto;
-         
-         aside.classList.remove("menu-toggle");
-         glass.classList.add('glass-hidden');
-
-   });
-}
-
-
-
-
-/**************************************/
-crearProductos(camisetasObj);
-crearProductos(campesObj);
-crearProductos(lompasObj);
-crearProductos(zapasObj);
--- js/productos.js
+++ js/productos.js
@@ -1,162 +1,142 @@
 /* CAMISETAS */
-let camisetasObj = [
+let productosObj = [
    {
       'id': "camiseta-01",
       'titulo': "Camiseta River Plate retro",
       'imagen': "./images/camisetas/camiseta river.jpg",
-      'categoria': "camiseta",
+      'categoria': "Camisetas",
       'precio': 35
    },
    {
       'id': "camiseta-02",
       'titulo': "Camiseta Boca Juniors retro",
       'imagen': "./images/camisetas/camiseta boca.jpg",
-      'categoria': "camiseta",
+      'categoria': "Camisetas",
       'precio': 35
    },
    {
       'id': "camiseta-03",
       'titulo': "Camiseta Barcelona retro",
       'imagen': "./images/camisetas/camiseta barca.jpg",
-      'categoria': "camiseta",
+      'categoria': "Camisetas",
       'precio': 45
    },
    {
       'id': "camiseta-04",
       'titulo': "Camiseta Real Madrid retro",
       'imagen': "./images/camisetas/camiseta realMadrid.jpg",
-      'categoria': "camiseta",
+      'categoria': "Camisetas",
       'precio': 50
    },
    {
       'id': "camiseta-05",
       'titulo': "Camiseta Argentina retro",
       'imagen': "./images/camisetas/camiseta argentina.jpg",
-      'categoria': "camiseta",
+      'categoria': "Camisetas",
       'precio': 30
    },
    {
       'id': "camiseta-06",
       'titulo': "Camiseta AC Milan retro",
       'imagen': "./images/camisetas/camiseta milan.jpg",
-      'categoria': "camiseta",
+      'categoria': "Camisetas",
       'precio': 45
    },
    {
       'id': "camiseta-07",
       'titulo': "Camiseta Inter Milan retro",
       'imagen': "./images/camisetas/camiseta inter.jpg",
-      'categoria': "camiseta",
+      'categoria': "Camisetas",
       'precio': 40
    },
-]
-/***************************************************************************************/
-
-
-
-
 /* CAMPERAS */
-let campesObj = [
    {
       'id': "campe-01",
       'titulo': "Campe Barça Nike",
       'imagen': "./images/campes/campe barca.jpg",
-      'categoria': "campe",
+      'categoria': "Campes",
       'precio': 80
    },
    {
       'id': "campe-02",
       'titulo': "Campe Fila retro",
       'imagen': "./images/campes/campe fila.jpg",
-      'categoria': "campe",
+      'categoria': "Campes",
       'precio': 70
    },
    {
       'id': "campe-03",
       'titulo': "Campe Re'suapre",
       'imagen': "./images/campes/campe resuafre.jpg",
-      'categoria': "campe",
+      'categoria': "Campes",
       'precio': 70
    },
    {
       'id': "campe-04",
       'titulo': "Campe Nike Clasic",
       'imagen': "./images/campes/campe nike.jpg",
-      'categoria': "campe",
+      'categoria': "Campes",
       'precio': 65
    },
    {
       'id': "campe-05",
       'titulo': "Campe Adidas",
       'imagen': "./images/campes/campe adidas.jpg",
-      'categoria': "campe",
+      'categoria': "Campes",
       'precio': 60
    },
-]
-/*********************************************************************************/
-
-
-
 /* PANTALONES */
-let lompasObj = [
    {
       'id': "lompa-01",
       'titulo': "Lompa Adidas",
       'imagen': "./images/lompas/lompa adidas.jpg",
-      'categoria': "lompa",
+      'categoria': "Lompas",
       'precio': 30
    },
    {
       'id': "lompa-02",
       'titulo': "Lompa Nike",
       'imagen': "./images/lompas/lompa nike.jpg",
-      'categoria': "lompa",
+      'categoria': "Lompas",
       'precio': 30
    },
    {
       'id': "lompa-03",
       'titulo': "Lompa The North Face",
       'imagen': "./images/lompas/lompa northFace.jpg",
-      'categoria': "lompa",
+      'categoria': "Lompas",
       'precio': 30
-   }
-]
-/******************************************************************/
-
-
-
+   },
 /* ZAPATILLAS */
-let zapasObj = [
    {
       'id': "zapas-01",
       'titulo': "Adidas Grand Court",
       'imagen': "./images/zapas/Adidas Grand Court.jpg",
-      'categoria': "zapas",
+      'categoria': "Zapas",
       'precio': 125
    },
    {
       'id': "zapas-02",
       'titulo': "Nike Air Max Plus",
       'imagen': "./images/zapas/Nike Air Max Plus.jpg",
-      'categoria': "zapas",
+      'categoria': "Zapas",
       'precio': 250
    },
    {
       'id': "zapas-03",
       'titulo': "Vans Nku Skool",
       'imagen': "./images/zapas/Vans Knu Skool.jpg",
-      'categoria': "zapas",
+      'categoria': "Zapas",
       'precio': 120
    },
    {
       'id': "zapas-04",
       'titulo': "Converse Chuck Taylor",
       'imagen': "./images/zapas/Converse.jpg",
-      'categoria': "zapas",
+      'categoria': "Zapas",
       'precio': 105
-   },
+   }
 ]
-
 
 /*********************************************************************/
 
