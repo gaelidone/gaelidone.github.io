f759616060fb72c1696fde05ee955bf83911c8b4
gaelidone <gaelidonesarmiento@gmail.com>
2024-03-23 00:16:37 -0300

    Se agregan nuevos productos

--- js/productos.js
+++ js/productos.js
@@ -2,85 +2,107 @@
 let productosObj = [
    {
       'id': "camiseta-01",
-      'titulo': "Camiseta River Plate retro",
+      'titulo': "Camiseta River Plate",
       'imagen': "./images/camisetas/camiseta river.jpg",
       'categoria': "Camisetas",
       'precio': 35
    },
    {
       'id': "camiseta-02",
-      'titulo': "Camiseta Boca Juniors retro",
+      'titulo': "Camiseta Boca Juniors",
       'imagen': "./images/camisetas/camiseta boca.jpg",
       'categoria': "Camisetas",
       'precio': 35
    },
    {
       'id': "camiseta-03",
-      'titulo': "Camiseta Barcelona retro",
+      'titulo': "Camiseta Barcelona",
       'imagen': "./images/camisetas/camiseta barca.jpg",
       'categoria': "Camisetas",
       'precio': 45
    },
    {
       'id': "camiseta-04",
-      'titulo': "Camiseta Real Madrid retro",
+      'titulo': "Camiseta Real Madrid",
       'imagen': "./images/camisetas/camiseta realMadrid.jpg",
       'categoria': "Camisetas",
       'precio': 50
    },
    {
       'id': "camiseta-05",
-      'titulo': "Camiseta Argentina retro",
+      'titulo': "Camiseta Argentina",
       'imagen': "./images/camisetas/camiseta argentina.jpg",
       'categoria': "Camisetas",
       'precio': 30
    },
    {
       'id': "camiseta-09",
-      'titulo': "Camiseta Brasil retro",
+      'titulo': "Camiseta Brasil",
       'imagen': "./images/camisetas/camiseta brasil.jpg",
       'categoria': "Camisetas",
       'precio': 35
    },
    {
       'id': "camiseta-06",
-      'titulo': "Camiseta AC Milan retro",
+      'titulo': "Camiseta AC Milan",
       'imagen': "./images/camisetas/camiseta milan.jpg",
       'categoria': "Camisetas",
       'precio': 45
    },
    {
       'id': "camiseta-07",
-      'titulo': "Camiseta Inter Milan retro",
+      'titulo': "Camiseta Inter Milan",
       'imagen': "./images/camisetas/camiseta inter.jpg",
       'categoria': "Camisetas",
       'precio': 35
    },
    {
       'id': "camiseta-08",
-      'titulo': "Camiseta Liverpool retro",
+      'titulo': "Camiseta Liverpool",
       'imagen': "./images/camisetas/camiseta liverpool.jpg",
       'categoria': "Camisetas",
       'precio': 55
    },
    {
       'id': "camiseta-10",
-      'titulo': "Camiseta Man United retro",
+      'titulo': "Camiseta Man United",
       'imagen': "./images/camisetas/camiseta manUnited.jpg",
       'categoria': "Camisetas",
       'precio': 50
    },
-/* CAMPERAS */
    {
-      'id': "campe-01",
-      'titulo': "Campe Barça Nike",
-      'imagen': "./images/campes/campe barca.jpg",
-      'categoria': "Campes",
-      'precio': 80
+      'id': "camiseta-11",
+      'titulo': "Camiseta Inglaterra",
+      'imagen': "./images/camisetas/camiseta inglaterra.jpg",
+      'categoria': "Camisetas",
+      'precio': 50
    },
    {
-      'id': "campe-02",
-      'titulo': "Campe Fila retro",
+      'id': "camiseta-12",
+      'titulo': "Camiseta Alemania",
+      'imagen': "./images/camisetas/camiseta alemania.jpg",
+      'categoria': "Camisetas",
+      'precio': 50
+   },
+   {
+      'id': "camiseta-13",
+      'titulo': "Camiseta Juventus",
+      'imagen': "./images/camisetas/camiseta juventus.jpg",
+      'categoria': "Camisetas",
+      'precio': 50
+   },
+
+/* CAMPERAS */
+{
+   'id': "campe-01",
+   'titulo': "Campe GRYM",
+   'imagen': "./images/campes/campe grym.jpg",
+   'categoria': "Campes",
+   'precio': 70
+},
+{
+   'id': "campe-02",
+      'titulo': "Fila retro",
       'imagen': "./images/campes/campe fila.jpg",
       'categoria': "Campes",
       'precio': 70
@@ -94,7 +116,7 @@ let productosObj = [
    },
    {
       'id': "campe-04",
-      'titulo': "Campe Nike WB",
+      'titulo': "Nike W&B",
       'imagen': "./images/campes/campe nikeWB.jpg",
       'categoria': "Campes",
       'precio': 65
@@ -113,6 +135,43 @@ let productosObj = [
       'categoria': "Campes",
       'precio': 70
    },
+   {
+      'id': "campe-07",
+      'titulo': "Nike Y2K",
+      'imagen': "./images/campes/campe nikeY2K.jpg",
+      'categoria': "Campes",
+      'precio': 70
+   },
+   
+   {
+      'id': "campe-08",
+      'titulo': "Barça Nike",
+      'imagen': "./images/campes/campe barca.jpg",
+      'categoria': "Campes",
+      'precio': 80
+   },
+   {
+      'id': "campe-09",
+      'titulo': "Real Madrid adidas",
+      'imagen': "./images/campes/campe realMadrid.jpg",
+      'categoria': "Campes",
+      'precio': 70
+   },
+   {
+      'id': "campe-10",
+      'titulo': "Adidas Green&White",
+      'imagen': "./images/campes/campe adidasVerde.jpg",
+      'categoria': "Campes",
+      'precio': 70
+   },
+   {
+      'id': "campe-11",
+      'titulo': "Campe Adidas retro",
+      'imagen': "./images/campes/campe adidasBlanca.jpg",
+      'categoria': "Campes",
+      'precio': 70
+   },
+   
 /* PANTALONES */
    {
       'id': "lompa-01",
@@ -130,40 +189,153 @@ let productosObj = [
    },
    {
       'id': "lompa-03",
-      'titulo': "Lompa The North Face",
+      'titulo': "Nike darkblue",
+      'imagen': "./images/lompas/lompa hellStar.jpg",
+      'categoria': "Lompas",
+      'precio': 30
+   },
+   {
+      'id': "lompa-04",
+      'titulo': "Lompa Nike blue",
+      'imagen': "./images/lompas/lompa rebookW.jpg",
+      'categoria': "Lompas",
+      'precio': 30
+   },
+   {
+      'id': "lompa-05",
+      'titulo': "Nike brown",
+      'imagen': "./images/lompas/lompa nikeMarron.jpg",
+      'categoria': "Lompas",
+      'precio': 30
+   },
+   {
+      'id': "lompa-06",
+      'titulo': "The North Face",
       'imagen': "./images/lompas/lompa northFace.jpg",
       'categoria': "Lompas",
       'precio': 30
    },
+   {
+      'id': "lompa-07",
+      'titulo': "Nike Barca",
+      'imagen': "./images/lompas/lompa barca.jpg",
+      'categoria': "Lompas",
+      'precio': 30
+   },
+   {
+      'id': "lompa-08",
+      'titulo': "Adidas Blue&White",
+      'imagen': "./images/lompas/lompa blanco.jpg",
+      'categoria': "Lompas",
+      'precio': 30
+   },
+   {
+      'id': "lompa-09",
+      'titulo': "Lompa Fila",
+      'imagen': "./images/lompas/lompa fila.jpg",
+      'categoria': "Lompas",
+      'precio': 30
+   },
+   {
+      'id': "lompa-10",
+      'titulo': "Cargo Joggin",
+      'imagen': "./images/lompas/lompa filaVerde.jpg",
+      'categoria': "Lompas",
+      'precio': 30
+   },
+   {
+      'id': "lompa-11",
+      'titulo': "Lompa UrbanX",
+      'imagen': "./images/lompas/lompa colorido.jpg",
+      'categoria': "Lompas",
+      'precio': 30
+   },
+
 /* ZAPATILLAS */
    {
-      'id': "zapas-01",
-      'titulo': "Adidas Grand Court",
-      'imagen': "./images/zapas/Adidas Grand Court.jpg",
-      'categoria': "Zapas",
-      'precio': 125
-   },
-   {
-      'id': "zapas-02",
-      'titulo': "Nike Air Max Plus",
-      'imagen': "./images/zapas/Nike Air Max Plus.jpg",
-      'categoria': "Zapas",
-      'precio': 250
-   },
-   {
-      'id': "zapas-03",
-      'titulo': "Vans Nku Skool",
-      'imagen': "./images/zapas/Vans Knu Skool.jpg",
+   'id': "zapas-01",
+      'titulo': "New Balance",
+      'imagen': "./images/zapas/zapas newBalance.jpg",
       'categoria': "Zapas",
       'precio': 120
    },
    {
-      'id': "zapas-04",
-      'titulo': "Converse Chuck Taylor",
-      'imagen': "./images/zapas/Converse.jpg",
+      'id': "zapas-02",
+      'titulo': "Puma Luxe Sport-180",
+      'imagen': "./images/zapas/zapas pumaLuxeSport180.jpg",
       'categoria': "Zapas",
-      'precio': 105
-   }
+      'precio': 175
+   },
+   {
+      'id': "zapas-03",
+      'titulo': "Nike M2K Tekno",
+      'imagen': "./images/zapas/zapas nikeM2K.jpg",
+      'categoria': "Zapas",
+      'precio': 220
+   },   
+   {
+      'id': "zapas-04",
+      'titulo': "Adidas Forum",
+      'imagen': "./images/zapas/zapas forum.webp",
+      'categoria': "Zapas",
+      'precio': 130
+   },   
+   {
+      'id': "zapas-05",
+      'titulo': "Street Sneakers",
+      'imagen': "./images/zapas/zapas asicsBlack.jpg",
+      'categoria': "Zapas",
+      'precio': 220
+   },
+   {
+      'id': "zapas-06",
+      'titulo': "Nike Air Max",
+      'imagen': "./images/zapas/Nike Air Max Plus.jpg",
+      'categoria': "Zapas",
+      'precio': 220
+   },
+   {
+      'id': "zapas-07",
+      'titulo': "Adidas KORN",
+      'imagen': "./images/zapas/Adidas Grand Court.jpg",
+      'categoria': "Zapas",
+      'precio': 115
+   },
+   {
+      'id': "zapas-08",
+      'titulo': "New Balance White",
+      'imagen': "./images/zapas/zapas newBalanceW.jpg",
+      'categoria': "Zapas",
+      'precio': 220
+   },
+   {
+      'id': "zapas-09",
+      'titulo': "Puma 220",
+      'imagen': "./images/zapas/zapas puma.webp",
+      'categoria': "Zapas",
+      'precio': 220
+   },
+   {
+      'id': "zapas-10",
+      'titulo': "Nike Air Max Zoom",
+      'imagen': "./images/zapas/zapas nike.jpg",
+      'categoria': "Zapas",
+      'precio': 250
+   },
+   {
+      'id': "zapas-11",
+      'titulo': "Asics Traditional",
+      'imagen': "./images/zapas/zapas asics.jpg",
+      'categoria': "Zapas",
+      'precio': 220
+   },
+   {
+      'id': "zapas-12",
+      'titulo': "Vans Knu Skool",
+      'imagen': "./images/zapas/Vans Knu Skool.jpg",
+      'categoria': "Zapas",
+      'precio': 130
+   },
 ]
 
 /*********************************************************************/
